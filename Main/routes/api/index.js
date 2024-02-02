25719a00357d2f30953813b259338ef5eb34428e
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-02-01 18:39:44 -0700

    Updated routes files

--- Main/routes/api/index.js
+++ Main/routes/api/index.js
@@ -1,8 +1,11 @@
+// Import express router, user-routes, and book-routes path
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const bookRoutes = require('./book-routes');
 
+// Tell express to use /users for the userRoutes and /books for the bookRoutes
 router.use('/users', userRoutes);
-router.use('/books', postRoutes);
+router.use('/books', bookRoutes);
 
+// Export express router
 module.exports = router;
