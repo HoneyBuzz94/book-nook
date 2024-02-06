df1333413b14d2cbb7877c647e21a9ce8c1fd735
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-02-06 12:41:41 -0700

    Updated journal view and route to enable the creation of new books in the db

--- routes/journal.js
+++ routes/journal.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('journal');
+    res.render('journal', { userId: req.session.user_id});
 })
 
 module.exports = router;
