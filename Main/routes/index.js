70114f1944f06a6f9a4e9cdf28ea547315da8ce9
James <jamesdwire66@gmail.com>
2024-02-05 20:55:02 -0700

    added handlebars

--- Main/routes/index.js
+++ Main/routes/index.js
@@ -2,10 +2,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homePage = require('./homepage');
+const login = require('./login');
 
 // Tell express to use /api for apiRoutes
 router.use('/api', apiRoutes);
 router.use('/', homePage);
-
+router.use('/login', login);
 // Export express router
 module.exports = router;
