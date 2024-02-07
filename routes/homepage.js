d44c8954bf3ccd3c5d44357c0bd4f89a10efccb9
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-02-06 18:42:04 -0700

    Added timestamps to books

--- routes/homepage.js
+++ routes/homepage.js
@@ -14,6 +14,7 @@ router.get("/", withAuth, async (req, res) => {
   try {
     const bookData = await Book.findAll({
       where: { user_id: req.session.user_id },
+      order: [['title', 'ASC']],
     });
 
     const books = bookData.map((project) => project.get({ plain: true }));
